900e8b73b6351704c135b4c9c9b0291a52a117b1
Lavish Sheth <109017996+lavishsheth@users.noreply.github.com>
2024-08-17 13:21:00 +0530

    commit

---
+++ Day 4/AB_Diff.java
@@ -0,0 +1,19 @@
+class AB_Diff {
+ public static void main(String args[]){
+    int arr[]={12,3,14,56,77,13};
+    int num=13;
+    int diff=2;
+    int n=arr.length;
+    System.out.println(findCount(arr,n,num,diff));
+ }   
+
+ public static int findCount(int arr[], int length, int num, int diff){
+    int count=0;
+    for(int i=0;i<length;i++){
+        if(Math.abs(arr[i]-num)<=diff){
+            count++;
+        }
+    }
+    return count;
+ }
+}
